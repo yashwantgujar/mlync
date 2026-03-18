d0c7317ef2828b86c7e232107db48e0b82d7e5d8
yashwant Gujar <162556387+yashwantgujar@users.noreply.github.com>
2026-03-18 18:20:05 +0530

    Update auth.routes.js

--- backend/Routes/auth.routes.js
+++ backend/Routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { registerUser, login } from "../Controllers/authController.js";
+import { registerUser, login ,socialLogin} from "../Controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 
-
+router.post("/social-login", socialLogin);
 router.post("/login", login);
 
 export default router;
